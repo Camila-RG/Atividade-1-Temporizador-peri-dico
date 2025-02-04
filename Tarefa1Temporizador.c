4c84c9c83489f4f0c0f76815bec74e393cc2c8bd
Camila-RG <camilaramosgomes@gmail.com>
2025-02-04 19:25:30 -0300

    Função de callback e temporizador adicionados ao código principal.

--- Tarefa1Temporizador.c
+++ Tarefa1Temporizador.c
@@ -1,31 +1,49 @@
 // Inclusão das bibliotecas necessárias
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/stdlib.h"
 #include "hardware/timer.h"
 
 // Definição dos pinos dos LEDs
-#define LED_G_PIN 11 // Correspondente a cor vermelho do semáforo
-#define LED_B_PIN 12 // Correspondente a cor amarelo do semáforo
-#define LED_R_PIN 13 // Correspondente a cor verde do semáforo
+#define LED_G_PIN 11 // Correspondente à cor verde do semáforo
+#define LED_B_PIN 12 // Correspondente à cor amarela do semáforo
+#define LED_R_PIN 13 // Correspondente à cor vermelha do semáforo
 
-void inicializacaoleds(){
+bool led_g_on = false; // Estado inicial do LED verde
+bool led_b_on = false; // Estado inicial do LED amarelo
+bool led_r_on = false; // Estado inicial do LED vermelho
+
+// Função para inicializar os pinos dos LEDs
+void inicializacaoleds() {
     gpio_init(LED_G_PIN);
     gpio_set_dir(LED_G_PIN, GPIO_OUT);
-    
+
     gpio_init(LED_B_PIN);
     gpio_set_dir(LED_B_PIN, GPIO_OUT);
 
     gpio_init(LED_R_PIN);
     gpio_set_dir(LED_R_PIN, GPIO_OUT);
 }
-int main()
-{
-    stdio_init_all();
-    inicializacaoleds();
 
-    while (true)
-    {
-        
+// Função de callback que será chamada repetidamente pelo temporizador
+bool repeating_timer_callback(struct repeating_timer *t) {
+    return true;
+}
+
+int main() {
+    stdio_init_all();
+    inicializacaoleds(); // Inicializa os LEDs
+
+    struct repeating_timer timer;
+
+    // Configuração do temporizador para chamar a função de callback a cada 3 segundos
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+
+    // Rotiina principal
+    while (true) {
+        // Contagem a cada segundo
+        sleep_ms(1000);
+        printf("Passou um segundo.\n");
     }
+
+    return 0;
 }
