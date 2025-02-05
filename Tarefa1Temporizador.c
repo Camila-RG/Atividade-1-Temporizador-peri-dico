56136a89ebdf4decf3e8303798049959e35fd010
Camila Gomes <camilaramosgomes@gmail.com>
2025-02-04 22:17:39 -0300

    Mudança de nome de variável

--- Tarefa1Temporizador.c
+++ Tarefa1Temporizador.c
@@ -5,7 +5,7 @@
 
 // Definição dos pinos dos LEDs
 #define LED_G_PIN 11 // Correspondente à cor verde do semáforo
-#define LED_B_PIN 12 // Correspondente à cor amarela do semáforo
+#define LED_Y_PIN 12 // Correspondente à cor amarela do semáforo
 #define LED_R_PIN 13 // Correspondente à cor vermelha do semáforo
 
 int semaforo_estado = 0; // Contador: 0 = Verde, 1 = Amarelo, 2 = Vermelho
@@ -15,8 +15,8 @@ void inicializacaoleds() {
     gpio_init(LED_G_PIN);
     gpio_set_dir(LED_G_PIN, GPIO_OUT);
 
-    gpio_init(LED_B_PIN);
-    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_init(LED_Y_PIN);
+    gpio_set_dir(LED_Y_PIN, GPIO_OUT);
 
     gpio_init(LED_R_PIN);
     gpio_set_dir(LED_R_PIN, GPIO_OUT);
@@ -27,19 +27,19 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 
      if (semaforo_estado == 0) {  // Estado vermelho
         gpio_put(LED_G_PIN, 1);
-        gpio_put(LED_B_PIN, 0);
+        gpio_put(LED_Y_PIN, 0);
         gpio_put(LED_R_PIN, 0);
 
         semaforo_estado = 1;  // Próximo estado: amarelo
     } else if (semaforo_estado == 1) {  // Estado amarelo
         gpio_put(LED_G_PIN, 0);
-        gpio_put(LED_B_PIN, 1);
+        gpio_put(LED_Y_PIN, 1);
         gpio_put(LED_R_PIN, 0);
 
         semaforo_estado = 2;  // Próximo estado: verde
     } else {  // Estado verde
         gpio_put(LED_G_PIN, 0);
-        gpio_put(LED_B_PIN, 0);
+        gpio_put(LED_Y_PIN, 0);
         gpio_put(LED_R_PIN, 1);
 
         semaforo_estado = 0;  // Retorna para o estado vermelho
